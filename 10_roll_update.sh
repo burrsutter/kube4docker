96b2be6874884f69b694834115c757563eb8b76c
Burr Sutter <bsutter@redhat.com>
2017-04-22 16:11:47 -0400

    updated to minishift rc2

--- 10_roll_update.sh
+++ 10_roll_update.sh
@@ -2,4 +2,4 @@
 
 # echo 'kubectl --namespace=kubedemo rolling-update mynode --image=burr/mynode:v2 --update-period=5s'
 # kubectl --namespace=kubedemo rolling-update myvertx --image=burr/myvertx:v2 --update-period=3s
-kubectl --namespace=kubedemo set image deployment/myvertx myvertx=burr/myvertx:v2
+kubectl --namespace=kubedemo set image deployment/myvertx myvertx=burr/myvertx:v1
--- 9_prepare_update.sh
+++ 9_prepare_update.sh
@@ -4,5 +4,6 @@ command='docker build -t burr/myvertx:v2 .'
 echo $command
 
 cd vertx-demo
+mvn package
 $command
 cd ..
