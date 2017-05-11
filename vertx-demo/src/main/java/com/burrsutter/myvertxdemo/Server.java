b1edf56f3623a135198fdf0e8b0d0e7d38e0fa87
Burr Sutter <bsutter@redhat.com>
2017-05-11 08:18:25 +0100

    updated initialDelaySeconds

--- vertx-demo/src/main/java/com/burrsutter/myvertxdemo/Server.java
+++ vertx-demo/src/main/java/com/burrsutter/myvertxdemo/Server.java
@@ -8,7 +8,7 @@ public class Server extends AbstractVerticle {
   public void start() {
     String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
     vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Bonjour Vert.x on " + hostname + " " + new java.util.Date()))
+        .requestHandler(req -> req.response().end("Hola Vert.x on " + hostname + " " + new java.util.Date()))
         .listen(8080);
   }
 
