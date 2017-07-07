b3d123665bea273d8b75f05a968a3308d154fb4b
Burr Sutter <bsutter@redhat.com>
2017-07-07 15:38:36 -0400

    adding Dockerfile for go demo

--- z_cleanup.sh
+++ z_cleanup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# in some cases you can just delete the deployment
+command1="kubectl delete deploy/myvertx --namespace=kubedemo"
+echo $command1
+$command1
+
+# in other cases you might wish to wipe the whole namespace
 echo 'kubectl delete -f ./kubedemo-namespace.yaml'
 kubectl delete -f ./kubedemo-namespace.yaml
